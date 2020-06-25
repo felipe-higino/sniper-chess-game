bddc1a7271b0bfe362666bb0fba4a94c1b5bd5f4
fhps <higino.felipe@hotmail.com>
2020-06-25 00:34:40 -0300

    Sniper instantiation method
    Organizing table as a 11x11 matrix and instantiating red snipers at the first row.

--- Sniper chess/Table/GridContainer.gd
+++ Sniper chess/Table/GridContainer.gd
@@ -1,13 +1,21 @@
 extends GridContainer
 
 export (PackedScene) var CellPrefab = null
+export (PackedScene) var SniperPrefab = null
 
 func _ready():
-	for i in 11*11:
-		var new_button = CellPrefab.instance()
-		new_button.index = i;
-		new_button.connect("cell_clicked", self, "_grid_pressed")
-		add_child(new_button)
+	for i in 11:
+		for j in 11:
+			#generate buttons
+			var new_button = CellPrefab.instance()
+			new_button.index = i;
+			new_button.connect("cell_clicked", self, "_grid_pressed")
+			add_child(new_button)
+			#generate snipers
+			if(i==0):
+				var sniper = SniperPrefab.instance()
+				sniper.set_team_red()
+				new_button.add_child(sniper)
 
 func _grid_pressed(buttonRef):
 	print(buttonRef.index)
